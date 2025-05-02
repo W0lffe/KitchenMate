f6708cad0017bba1818c13f0bf1046b6daab15b3
Henry <henry.karppinen1997@gmail.com>
2025-05-02 12:54:43 +0300

    Changed button to icon

--- src/components/List/ListItem.jsx
+++ src/components/List/ListItem.jsx
@@ -2,6 +2,9 @@ import { KitchenContext } from "../../context/KitchenContext"
 import { useContext } from "react"
 import { listItemStyle, 
         listItemNameStyle } from "./listStyles"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function ListItem({item}){
 
@@ -19,7 +22,7 @@ export default function ListItem({item}){
             <label className={listItemNameStyle}>{item.name}</label>
             <label>{item.portions}</label>
             <label>{item.prepTime}</label>
-            <button onClick={handleClick}>Icon</button>
+            <button onClick={handleClick}><FontAwesomeIcon icon={faEye} /></button>
         </li>
     )
 }
