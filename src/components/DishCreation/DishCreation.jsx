70014e17245d95cfee93b161d6fb57781309317a
Henry <henry.karppinen1997@gmail.com>
2025-05-24 21:57:35 +0300

    Dish creation continuation

--- src/components/DishCreation/DishCreation.jsx
+++ src/components/DishCreation/DishCreation.jsx
@@ -1,23 +1,64 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { KitchenContext } from "../../context/KitchenContext"
 import SubmitButton from "../Buttons/SubmitButton"
 import DishInfoSection from "./DishInfoSection"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSquarePlus } from "@fortawesome/free-solid-svg-icons"
 
 export default function DishCreation(){
 
-    const {isMobile, setModalState} = useContext(KitchenContext)
+    const {isMobile, setModalState, activeDish, availableRecipes, setActiveDish} = useContext(KitchenContext)
+    const [components, setComponents] = useState([])
 
+    const isCreatingDish = activeDish?.mode === "create";
     const mobileHeading = "Dish Creation"
 
+    useEffect(() => {
+        if(isCreatingDish && activeDish.components){
+            setComponents([...activeDish?.components])
+        }
+    }, [isCreatingDish, activeDish?.components])
+
+    const addComponent = (item) => {
+        if(isCreatingDish){
+                const existingComponents = activeDish?.components || [];
+                const components = [...existingComponents, item];
+               
+                setActiveDish({dish: null, mode: "create", components})
+            }
+    }
+
     return(
-        <div>
+        <div className="text-white">
            {isMobile ? 
-                       <span className="flex flex-row justify-end items-center px-2 text-white">
-                           <h2 className="">{mobileHeading}</h2>
+                       <span className="flex flex-row justify-end items-center p-2">
+                           <h2 className="w-full text-center">{mobileHeading}</h2>
                            <SubmitButton use={"close"} func={setModalState} />
                        </span> 
             : null}
             <DishInfoSection />
+            {isMobile ? <div className="flex flex-col list-none w-full max-h-50 h-50 overflow-y-auto gap-1 items-center border-b-gray-400/40 border-b rounded-custom mt-3">
+                <label>Add Recipes to Dish</label>
+                <span className="flex flex-col gap-2 h-full w-4/6 items-start">
+                    {availableRecipes.map((recipe, i) => 
+                    <li key={i} className="flex flex-row w-full justify-between">
+                        <label>{recipe.name}</label>
+                        <FontAwesomeIcon icon={faSquarePlus}
+                                        onClick={() => addComponent(recipe)}/>
+                    </li>)}
+                </span>
+                
+            </div> : null}
+            <div className="flex flex-col list-none w-full max-h-50 h-50 overflow-y-auto gap-1 items-center border-b-gray-400/40 border-b rounded-custom mt-3">
+                <label>Components</label>
+                <span className="flex flex-col gap-2 h-full w-4/6 items-start">
+                    {components.length > 0 ? components.map((component, i) => <li key={i}>{component.name}</li>) 
+                    : <p>No components added yet.</p>}
+                </span>
+            </div>
+            <footer className="flex items-center justify-center m-2">
+                <SubmitButton use="recipe"/>
+            </footer>
         </div>
     )
 }
--- src/components/DishCreation/DishInfoSection.jsx
+++ src/components/DishCreation/DishInfoSection.jsx
@@ -3,19 +3,24 @@ import { courses } from "../../util/util"
 
 export default function DishInfoSection(){
     return(
-        <div>   
-             <span>
-                <label>Name:</label>
-                <input type="text" name="name" placeholder="Name for dish" />
-            </span>
-             <span>
-                <label>Course:</label>
-                <select>
-                    <option value="course">Select course</option>
-                    {courses.map((course) => <option value={course} key={course}>{course}</option>)}
-                </select>
-            </span>
-            
+        <div className="flex flex-col lg:flex-row w-full p-2 gap-5 mt-1">
+            <section className="flex flex-col lg:w-1/2 gap-2">
+                <span className="flex flex-row gap-3">
+                    <label>Name:</label>
+                    <input type="text" name="name" placeholder="Name for dish" />
+                </span>
+                <span className="flex flex-row gap-3">
+                    <label>Course:</label>
+                    <select>
+                        <option value="course">Select course</option>
+                        {courses.map((course) => <option value={course} key={course}>{course}</option>)}
+                    </select>
+                </span>
+            </section>
+            <section className="flex flex-row lg:w-1/2 gap-2">
+                <label>Image:</label>
+                <input type="file" />
+            </section>
         </div>
     )
 }
