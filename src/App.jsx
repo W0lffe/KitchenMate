0f9380641e0e9eeb663714e889d636e307bf8ce0
Henry <henry.karppinen1997@gmail.com>
2025-10-10 23:17:09 +0300

    refactored

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,9 @@ import MainContainer from './components/MainContainer/MainContainer'
 import NavigationWrapper from './components/Sidebar/NavigationWrapper'
 import KitchenContextProvider from './context/KitchenContext'
 import { Toaster } from 'react-hot-toast'
-import { REFACTOR } from '../backend/api'
 
 export default function App() {
-  REFACTOR();
+
   return (
     <KitchenContextProvider>
       <div className="flex justify-center items-start w-full h-full">
