ef0f1242d833916e40e7c739859a912b15ca77e3
Henry <160464746+W0lffe@users.noreply.github.com>
2025-12-01 21:38:10 +0200

    59 page for password reset (#63)
    * Reset password page started, modified Jenkinsfile
    
    * updated README
    
    * updated jenkins
    
    * updated readme
    
    * added more logic to password reset hook
    
    * Able to reset password

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -4,19 +4,30 @@ import { inputStyle, labelStyle } from "../components/LoginForm/loginStyles";
 import { useActionState, useContext } from "react";
 import { KitchenContext } from "../context/KitchenContext";
 import useResetPassword from "../hooks/useResetPassword";
+import RecCode from "../components/RecCode/RecCode";
 
 export default function ResetPassword(){
     const {handleRequest} = useContext(KitchenContext);
     const resetPassword = useResetPassword({handleRequest});
-    const [formState, formAction] = useActionState(resetPassword , {success: true});
-    
+    const [formState, formAction] = useActionState(resetPassword , {});
 
+    if(formState?.code){
+        return(
+            <div className="w-full h-full flex items-center justify-center text-black">
+                <section className="bg-white/90 flex items-center justify-center flex-col p-6 gap-5 border rounded-custom-low">
+                    <h3 className="font-semibold">Password changed successfully!</h3>
+                    <RecCode code={formState.code} buttonMsg={"Back to App"}/>
+                </section>
+            </div>
+        )
+    }
+    
     return(
         <div className="w-full h-full flex items-center justify-center text-black">
             <div className="bg-white/90 flex items-center justify-center flex-col p-6 gap-5 border rounded-custom-low">
                 <h2 className="text-xl font-bold">Reset Password</h2>
                 <form action={formAction} className="flex flex-col gap-3 items-center">
-                    {formState.success ? (
+                    {formState?.success ? (
                         <>
                             <label className={labelStyle}>Enter new password</label>
                             <CredInput isPass={true} state={{}} />
